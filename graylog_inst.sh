b4ad9c5d2129a49c22901a01af1b4fe0463c117d
GamjaPower <sst9696@gmail.com>
2018-12-01 07:49:48 +0900

    Create graylog_inst.sh

---
+++ graylog_inst.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+
+# echo "* soft nofile 65000" | sudo tee --append /etc/security/limits.conf
+# echo "* soft nofile 65000" | sudo tee --append /etc/security/limits.conf
+
+sudo apt -y update 
+sudo apt -y install apt-transport-https openjdk-8-jre-headless uuid-runtime pwgen
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+sudo apt -y update
+sudo apt -y install mongodb-org
+
